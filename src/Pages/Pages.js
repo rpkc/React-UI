147516b80050b072340f5b13f2620cda36384af1
rpkc <rupakc770@gmail.com>
2023-07-14 10:47:54 +0530

    Some pages are Deleted 14.07.23

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -1,19 +1,8 @@
 import Page404 from './404';
-import BasicCalc from './BasicCalc';
-import Converter from './Converter';
 import NumberSystem from './NumberSystem';
 import Temp from './Temp';
 import Semi404 from './Semi404';
 
-
-//form Basic Calc
-import Factor from './Basic Calc/Factor';
-import HCF from './Basic Calc/HCF';
-import LCM from './Basic Calc/LCM';
-
-//Converter
-import Temprature from './Converter/Temprature';
-import Length from './Converter/Length';
 
 //Number System
 import BaseCalc from './Number System/BaseCalc';
@@ -21,4 +10,4 @@ import BaseConv from './Number System/BaseConv';
 
 
 
-export {Page404,BasicCalc,Converter,Temp,Factor,HCF,LCM,Temprature,Length,BaseCalc,BaseConv,NumberSystem,Semi404};
+export {Page404,Temp,BaseCalc,BaseConv,NumberSystem,Semi404};
