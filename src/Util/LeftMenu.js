4912b27b3698a915460a3b89c382b62dcced66d0
rpkc <rupakc770@gmail.com>
2023-08-08 16:03:41 +0530

    Animation Modified on 08-08-2023

--- src/Util/LeftMenu.js
+++ src/Util/LeftMenu.js
@@ -4,7 +4,6 @@ import React from "react";
 import '../Style/LeftMenu.css';
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-import { motion } from 'framer-motion';
 
 export default function LeftMenu(props){
     const [openLeftPane,setOpenLeftPane]=useState(true); // open-close menu
--- src/Util/RightMain.js
+++ src/Util/RightMain.js
@@ -2,16 +2,15 @@
 // Sidenav Mobile Menu 
 import React from "react";
 import {Outlet} from "react-router-dom";
-import { motion } from 'framer-motion';
 export default function RightMain()
 {
     return(
-        <motion.div className="right-pane" 
+        <div className="right-pane" 
         initial={{opacity:0,transition:{duration:.5}}}
         animate={{opacity:1,transition:{duration:.5}}}
         exit={{opacity:0,transition:{duration:.5}}}
         >
             <Outlet/>
-        </motion.div>
+        </div>
     );
 }
