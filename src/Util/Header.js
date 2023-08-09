74867f0adeafeb3f42584b65ebabcbc0fae804c4
rpkc <rupakc770@gmail.com>
2023-08-09 13:14:55 +0530

    Main Menu rearranged on 09-08-2023

--- src/Util/Header.js
+++ src/Util/Header.js
@@ -15,9 +15,9 @@ return(
     <div className="menu">
         <div className="header">C A L C U L O N</div>
         <ul className="menu-list">
-          <li className="menu-item"><NavLink to='/calculator' info="Calculators" activeClassName="active"><i class="fa fa-calculator" aria-hidden="true"></i></NavLink></li>
           <li className="menu-item"><NavLink to='/archive' info="Archives" activeClassName="active"><i class="fa fa-archive" aria-hidden="true"></i></NavLink></li>
-          <li className="menu-item"><NavLink to='/projects' info="Projects" activeClassName="active"><i class="fa fa-microchip" aria-hidden="true"></i></NavLink></li>
+          <li className="menu-item"><NavLink to='/projects' info="Embedded" activeClassName="active"><i class="fa fa-microchip" aria-hidden="true"></i></NavLink></li>
+          <li className="menu-item"><NavLink to='/calculator' info="Hosting" activeClassName="active"><i class="fa fa-server" aria-hidden="true"></i></NavLink></li>
           <li className="menu-item"><button className="menuButton" onClick={openMenu} info="Menu"><i class="fa fa-th" aria-hidden="true"></i></button></li>
         </ul>
         <div id="miniNav" class="mininav" tabIndex={0}>
