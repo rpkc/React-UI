d76e2ce9cc39a13999f980e29280f2b2557f5906
rpkc <rupakc770@gmail.com>
2023-08-09 10:08:32 +0530

    some LeftMenu.css adjustments & RightMain.js striping on 09-08-2023

--- src/Util/RightMain.js
+++ src/Util/RightMain.js
@@ -5,11 +5,7 @@ import {Outlet} from "react-router-dom";
 export default function RightMain()
 {
     return(
-        <div className="right-pane" 
-        initial={{opacity:0,transition:{duration:.5}}}
-        animate={{opacity:1,transition:{duration:.5}}}
-        exit={{opacity:0,transition:{duration:.5}}}
-        >
+        <div className="right-pane">
             <Outlet/>
         </div>
     );
